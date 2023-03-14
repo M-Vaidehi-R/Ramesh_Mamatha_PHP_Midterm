54ef4c95f315f08b161806888ee8b020db1bb0a6
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2023-03-14 15:11:57 -0400

    url change

--- views/go_back.php
+++ views/go_back.php
@@ -1,4 +1,4 @@
 <div class="new_user_btn_box">
     <h3 hidden>Go back to main Page</h3>
-    <a class="new_user_btn" href="http://localhost/midterm-oop">Go Back >></a>
+    <a class="new_user_btn" href="http://localhost/Ramesh_Mamatha_PHP_Midterm">Go Back >></a>
 </div>
